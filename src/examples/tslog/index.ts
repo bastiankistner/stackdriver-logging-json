8bbf7a7696fb0773cc537d9b8f1a0206fa7ca2c9
Bastian Kistner <bastian.kistner@gmail.com>
2020-11-17 20:49:50 +0100

    creating example

--- src/examples/tslog/index.ts
+++ src/examples/tslog/index.ts
@@ -1,8 +1,54 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // TODO
 
 import { auth } from 'google-auth-library';
+import { Logger } from 'tslog';
+import { SEVERITY_KEY } from 'types/entry.common';
+
+function pullFirst<T>(args: any[], predicate: (value: any, index: number, obj: any[]) => boolean) {
+	const index = args.findIndex(predicate);
+
+	if (index === -1) {
+		return [undefined, args];
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+	return [args[index] as T, [...args.slice(0, index), ...args.slice(index + 1)]];
+}
+
+export function createEntry(severity: SEVERITY_KEY, ...args: any[]) {
+	// analyse args
+
+	// 1. find the first error object if available
+
+	const [error, args] = pullFirst<Error>(args, (v) => v instanceof Error);
+
+	return {};
+}
+
+function write(data: Record<string, any>) {
+	process.stdout.write(JSON.stringify(data));
+}
 
 export async function initLogger() {
 	// use google-auth-library to determine projectId (when run on a GCP resource)
 	const projectId = await auth.getProjectId();
+
+	console.log({ projectId });
+
+	const logger = new Logger({ suppressStdOutput: true });
+
+	logger.attachTransport({
+		silly: (...args) => write(createEntry('DEFAULT', args)),
+		trace: (...args) => write(createEntry('DEBUG', args)),
+		debug: (...args) => write(createEntry('DEBUG', args)),
+		info: (...args) => write(createEntry('INFO', args)),
+		warn: (...args) => write(createEntry('WARNING', args)),
+		error: (...args) => write(createEntry('ERROR', args)),
+		fatal: (...args) => write(createEntry('EMERGENCY', args)),
+	});
 }
+
+void initLogger();
+
+console.log('ok');
--- src/types/entry.common.ts
+++ src/types/entry.common.ts
@@ -1,5 +1,7 @@
 import { SEVERITY } from '../constants';
 
+export type SEVERITY_KEY = keyof typeof SEVERITY;
+
 export type CommonEntryMetadata = {
 	/**
 	 * logName can be set for each entry (e.g. to say std_err and std_out)
@@ -7,5 +9,5 @@ export type CommonEntryMetadata = {
 	logName?: string;
 
 	// ✅
-	severity?: keyof typeof SEVERITY;
+	severity?: SEVERITY_KEY;
 };
